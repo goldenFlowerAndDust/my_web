ff40337a58bc001e5be6abb7fda9826efbab153a
BlossomsAndDust学习者 <3289325372@qq.com>
2026-03-09 21:08:20 +0800

    删除不必要的

---
+++ 内容文件夹/自建本地网站/导航栏内容/课后作业/JavaScript/第一章/第一节课.js
@@ -0,0 +1,16 @@
+let name = prompt("你的名字是：")
+let age = prompt("今年多少岁：")
+
+
+alert("你好：" + name  + "\n" + age + "岁")
+
+
+document.write("欢迎来到JavaScript")
+
+console.log("普通信息")
+console.info("提示类信息")
+console.error("错误信息")
+console.warn("警示信息")
+
+
+
---
+++ 内容文件夹/自建本地网站/导航栏内容/课后作业/JavaScript/第一章/练习.js
@@ -0,0 +1,5 @@
+
+let dizhi = prompt("你的家乡在哪里？")
+alert("你的家乡在：" + dizhi)
+
+alert("祝大家！\n学业有成！\n共同进步！")
